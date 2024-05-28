b470ed6887b61b51d4bc457899f4a1f63cbd2514
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-05-28 11:33:15 +0500

    Issue 5 - Convert areas array to areas tree

--- app/models/page.tsx
+++ app/models/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import prisma from "@/prisma/client";
+import React from "react";
+import _ from "lodash";
+import { Asset as Base } from "@prisma/client";
 
-const ModelsPage = () => {
-  return (
-    <div>ModelsPage</div>
-  )
+interface Asset extends Base {
+  children: Asset[];
 }
 
-export default ModelsPage
+const ModelsPage = async () => {
+  const areasTree = await getAreasTree();
+
+  return <div>ModelsPage</div>;
+};
+
+const getAreasTree = async (): Promise<Asset | null> => {
+  const AreaType = await prisma.assetType.findUnique({
+    where: { name: "area" },
+  });
+  if (!AreaType) return null;
+
+  const areas = await prisma.asset.findMany({
+    where: { assetTypeId: AreaType?.id },
+    orderBy: { id: "asc" },
+  });
+
+  const map: { [key: number]: Asset } = {};
+  areas.forEach(
+    (area) => (map[area.id] = { ...area, children: [] as Asset[] })
+  );
+
+  let root: Asset | null = null;
+
+  areas.forEach((area) => {
+    if (!area.parentAssetId) root = map[area.id];
+    else if (map[area.parentAssetId])
+      map[area.parentAssetId].children.push(map[area.id]);
+  });
+
+  return root;
+};
+
+export default ModelsPage;
